3de3945c51ee6e0c8ffe65f87530e8441392d0c5
@Fe_18_GSTU_Nickolay_Rusinovich <nicolyarusinovich77@gmail.com>
2019-07-02 17:26:14 +0300

    change

--- src/main/java/com/rusinovich/todolist/service/TaskService.java
+++ src/main/java/com/rusinovich/todolist/service/TaskService.java
@@ -1,20 +1,39 @@
 package com.rusinovich.todolist.service;
 
+import com.rusinovich.todolist.dao.TaskDao;
 import com.rusinovich.todolist.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Service class for {@link com.rusinovich.todolist.model.Task}
  */
+@Service
+@Transactional(readOnly = true)
+public class TaskService {
 
-public interface TaskService {
+    @Autowired
+    private TaskDao taskDao;
 
-    void save(Task task);
+    @Transactional
+    public void save(Task task) {
+        taskDao.save(task);
+    }
 
-    Task findById(long id);
+    public Task findById(long id) {
+        return taskDao.findById(id);
+    }
 
-    List<Task> findByUserId(long user_id);
+    @Transactional
+    public void delete(long id){
+        taskDao.delete(id);
+    }
 
-    void delete(long id);
+    public List<Task> findByUserId(long user_id){
+
+        return taskDao.findByUser_id(user_id);
+    }
 }
